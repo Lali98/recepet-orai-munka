ad121727901bb7ccf877e15eec5e756e7fdd1b72
Lali98 <kisslajosbertalan77@gmail.com>
2023-03-01 09:25:47 +0100

    2023.03.01.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, NavLink, Redirect, Route, Switch} from "react-router-dom";
+import { Home } from './components/Home';
+import { RecipeList } from './components/RecipeList';
+import { RecipeCreate } from './components/RecipeCreate';
+import { RecipeEdit } from './components/RecipeEdit';
+import { RecipeSingle } from './components/RecipeSingle';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router forceRefresh>
+            <nav className='navbar fixed-top navbar-expand-sm navbar-dark bg-primary mb-3'>
+                <div className='' id='navbarNav'>
+                    <ul className='navbar-nav lead font-weight-bold'>
+                        <li className='nav-item'>
+                            <NavLink to={'/'}>
+                                <span className='nav-link'>
+                                    <img width='60px' src={`${process.env.REACT_APP_BACKEND_URL}/static/assets/logo.png`} alt=""/>
+                                </span>
+                            </NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink to={'/receptek'} activeClassName='active' exact>
+                                <span className='nav-link pt-4'>
+                                    Receptek
+                                </span>
+                            </NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink to={'/új-recept'} activeClassName='active' exact>
+                                <span className='nav-link pt-4'>
+                                    Új recept
+                                </span>
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div className='container' style={{marginTop: '140px'}}>
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/receptek' component={RecipeList} />
+                    <Route exact path='/uj-recept' component={RecipeCreate} />
+                    <Route exact path='/recept-szerkesztes/:recipeSlug' component={RecipeEdit} />
+                    <Route exact path='/recept:/recipeSlug' component={RecipeSingle} />
+                    <Redirect to={'/'} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/RecipeList.js
@@ -0,0 +1,54 @@
+import {Fragment, useEffect, useState} from "react";
+import {Spinner} from "./Spinner";
+import {NavLink} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPencilAlt} from "@fortawesome/free-solid-svg-icons";
+
+export function RecipeList() {
+    const [recipes, setRecipes] = useState([]);
+    const [isPending, setPending] = useState(false);
+    function fetchRecipes() {
+        return fetch(process.env.REACT_APP_BACKEND_URL + "/api/recipes")
+            .then((res) => res.json())
+            .then(setRecipes);
+    }
+    useEffect(() => {
+        setPending(true);
+        fetchRecipes().finally(() => {
+            setPending(false);
+        });
+    }, []);
+    if(isPending) {
+        return <Spinner />;
+    }
+    return (
+        <Fragment>
+            {alignToRows(recipes).map((row, i) => (
+                <div className='row mb-3' key={i}>
+                    {row.map((recipe) => (
+                        <div className='col-md-4' key={recipe.id}>
+                            <div className='card w-100 h-100'>
+                                <img className='card-img-top mb-2' src={`${process.env.REACT_APP_BACKEND_URL}/static/images/${recipe.imageURL}`} alt=""/>
+                                <div className='card-body'>
+                                    <h5 className='card-title'>{recipe.name}</h5>
+                                </div>
+                                <div className='w-75 p-3'>
+                                    <NavLink to={`/recept-szerkesztes/${recipe.slug}`}>
+                                        <button className='btn btn-sm btn-outline-warning mr-2'>
+                                            <FontAwesomeIcon icon={faPencilAlt} />
+                                        </button>
+                                    </NavLink>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </Fragment>
+    )
+}
+
+const alignToRows = (items) => items.slice().reduceRight((acc, curr, i, arr) => {
+    acc.push(arr.splice(0, 3));
+    return acc;
+}, []);
